74cb768646d0f8d95c71bf67b5ca207bbbacf656
Philipp Balzarek <p.balzarek@googlemail.com>
2017-08-04 12:18:26 +0200

    update to LTS-9.0

--- src/Github.hs
+++ src/Github.hs
@@ -4,15 +4,19 @@
 
 module Github where
 
-import           Control.Applicative ((<$>))
+import           Control.Applicative    ((<$>))
 import           Control.Monad
 import           Control.Monad.Trans
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import qualified Data.Foldable as Foldable
-import           Github.Auth
-import           Github.Repos
-import           System.Exit (exitSuccess)
+import qualified Data.Foldable          as Foldable
+import qualified Data.Text              as T
+import qualified Data.Text              as Text
+import qualified Data.Text.Encoding     as T
+import qualified GitHub.Auth            as GitHub
+import qualified GitHub.Data.Name       as GitHub
+import qualified GitHub.Data.Repos      as GitHub
+import qualified GitHub.Data.URL        as GitHub
+import qualified GitHub.Endpoints.Repos as GitHub
+import           System.Exit            (exitSuccess)
 import           System.Timeout
 
 import           IO
@@ -31,13 +35,16 @@ maybeClone repo dir = unlessConf "github.enable" (== False) $
             do cloneP <- promptYesNo "clone"
                                      "A repository with this name already exists\
                                      \in your github account. Clone it instead?"
-               when cloneP $ do run "git" ["clone", rUrl, T.unpack dir]
+               when cloneP $ do run "git" [ "clone"
+                                          , Text.unpack $ GitHub.getUrl rUrl
+                                          , T.unpack dir]
                                 liftIO exitSuccess
         _ -> return ()
   where
     toSeconds Nothing = 5 * 10^(6::Int)
     toSeconds (Just t) = t * 10^(6::Int)
-    getRepo uname = fmap repoSshUrl <$> userRepo (T.unpack uname) (T.unpack repo)
+    getRepo uname = fmap GitHub.repoSshUrl
+                    <$> GitHub.repository (GitHub.N uname) (GitHub.N repo)
 
 -- | Create a github repository in the users account and set it as origin if
 -- permitted by configuration
@@ -46,28 +53,29 @@ handleGithub repo description = unlessConf "github.enable" (== False) $
                                 unlessConf "github.never-create" (== True) $
                                 unlessEmptyPrompt "repository" $ \rName ->  do
     auth <- confLookup "github.auth.oauth" >>= \case
-        Just oauth -> return $ GithubOAuth oauth
+        Just oauth -> return $ GitHub.OAuth oauth
         Nothing -> do uname <- confOrPrompt "github.username" "Github Username" ""
                       pwd <- confOrPrompt "github.auth.password"
                                           "Github Password"
                                           ""
-                      return $ GithubBasicAuth (T.encodeUtf8 uname)
-                                               (T.encodeUtf8 pwd)
-    let nRepo = NewRepo { newRepoName         = T.unpack rName
-                        , newRepoDescription  = Just (T.unpack description)
-                        , newRepoHomepage     = Nothing
-                        , newRepoPrivate      = Nothing
-                        , newRepoHasIssues    = Nothing
-                        , newRepoHasWiki      = Nothing
-                        , newRepoAutoInit     = Nothing }
+                      return $ GitHub.BasicAuth (T.encodeUtf8 uname)
+                                                (T.encodeUtf8 pwd)
+    let nRepo = GitHub.NewRepo
+                { GitHub.newRepoName         = GitHub.N rName
+                , GitHub.newRepoDescription  = Just description
+                , GitHub.newRepoHomepage     = Nothing
+                , GitHub.newRepoPrivate      = Nothing
+                , GitHub.newRepoHasIssues    = Nothing
+                , GitHub.newRepoHasWiki      = Nothing
+                , GitHub.newRepoAutoInit     = Nothing }
     liftIO $ putStrLn "Creating github repository"
-    mbRepo <- liftIO $ createRepo auth nRepo
+    mbRepo <- liftIO $ GitHub.createRepo' auth nRepo
     case mbRepo of
         Left e -> do liftIO $ putStrLn $ "An Error occured while trying to create the repository:" ++ show e
         Right r -> do liftIO $ putStrLn "Done."
                       liftIO $ putStrLn "Configuring git to track the new repository"
-                      Foldable.forM_ (repoSshUrl r) $ \url ->
-                          run "git" ["remote", "add", "origin", url]
+                      Foldable.forM_ (GitHub.repoSshUrl r) $ \url ->
+                          run "git" ["remote", "add", "origin", Text.unpack $ GitHub.getUrl url]
                       run "git" ["config", "branch.master.remote", "origin"]
                       run "git" [ "config"
                                 , "branch.master.merge"
